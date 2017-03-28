38cb4490221c9621cde0ae641de10d859f05ed9c
Andrew MacMurray <a.macmurray@icloud.com>
2017-03-28 16:35:27 +0100

    10 to string

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :logger, level: :info
 config :what3things, What3things.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || 10),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
 # ## SSL Support
