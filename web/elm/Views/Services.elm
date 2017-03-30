cd4ecbff87a07e7674f32570831278822e90138a
Andrew MacMurray <a.macmurray@icloud.com>
2017-03-30 12:27:46 +0100

    rough styling for results #29

--- web/elm/Views/Services.elm
+++ web/elm/Views/Services.elm
@@ -3,22 +3,26 @@ module Views.Services exposing (services)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Model exposing (..)
+import Components.Logo exposing (logo)
 
 
 services : Model -> Html Msg
 services model =
     div []
-        [ h2 [] [ text <| renderPageTitle model ]
-        , div [] (List.map renderService model.services)
+        [ logo
+        , div [ class "bg-blue" ]
+            [ h2 [ class "blue" ] [ text <| renderPageTitle model ]
+            , div [] (List.map renderService model.services)
+            ]
         ]
 
 
 renderService : Service -> Html Msg
 renderService service =
-    div []
-        [ p [] [ text service.title ]
+    div [ class "bg-white ma4 pa3" ]
+        [ p [ class "blue f3" ] [ text service.title ]
         , p [] [ text service.body ]
-        , a [ href service.url ] [ p [] [ text service.cta ] ]
+        , a [ href service.url, class "white no-underline dib center" ] [ p [ class "bg-dark-blue ph3 pv2" ] [ text service.cta ] ]
         ]
 
 
