ecfaf583147091fab1f803b2f8d3a926f9d13614
Andrew MacMurray <a.macmurray@icloud.com>
2017-04-04 11:40:44 +0100

    remove uneccesary render functions in QuoteView

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,11 +1,3 @@
 defmodule What3things.LayoutView do
   use What3things.Web, :view
-
-  def index(conn, _params) do
-    render conn, "app.html"
-  end
-
-  def admin(conn, _params) do
-    render conn, "admin.html"
-  end
 end
