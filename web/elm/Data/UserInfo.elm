808489ba25b3ae19365acaa349eb2c874d263454
Andrew MacMurray <a.macmurray@icloud.com>
2017-04-06 12:12:39 +0100

    link to SO article on postcode regex

--- web/elm/Data/UserInfo.elm
+++ web/elm/Data/UserInfo.elm
@@ -60,6 +60,10 @@ validatePostcode postcode =
         Invalid postcode
 
 
+
+-- regex from Stack overflow answer: http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
+
+
 postcodeRegex : Regex
 postcodeRegex =
     regex "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$"
