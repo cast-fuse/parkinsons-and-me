9bf3de89b606c9fdf3fb51fa7c2159d0ec4dfc0e
Andrew MacMurray <a.macmurray@icloud.com>
2017-04-07 12:57:41 +0100

    separate into test helpers

--- test/elm/WeightingsSpec.elm
+++ test/elm/WeightingsSpec.elm
@@ -2,6 +2,7 @@ module WeightingsSpec exposing (..)
 
 import Test exposing (..)
 import Expect
+import TestHelpers exposing (..)
 import Model exposing (..)
 import Update exposing (..)
 import Data.Weightings exposing (..)
@@ -29,7 +30,7 @@ handleAnswerSpec =
 secondQuoteModel : Model
 secondQuoteModel =
     { firstQuoteModel
-        | userWeightings = Dict.fromList [ ( 0, 1.6 ), ( 1, 1.8 ), ( 2, 0.6 ) ]
+        | userWeightings = doubleWeightingsDict
     }
 
 
@@ -38,8 +39,8 @@ firstQuoteModel =
     { initialModel
         | userWeightings = dummyWeightingsDict
         , weightings = dummyWeightings
-        , currentQuote = Just 0
-        , remainingQuotes = Just [ 1, 2 ]
+        , currentQuote = Just 1
+        , remainingQuotes = Just [ 2, 3 ]
     }
 
 
@@ -57,28 +58,3 @@ addWeightingsSpec =
         [ test "takes 2 weightingsDict and combines their values by adding them together" <|
             \() -> Expect.equal (addWeightings zerosWeightingDict dummyWeightingsDict) dummyWeightingsDict
         ]
-
-
-dummyWeightings : Weightings
-dummyWeightings =
-    Dict.fromList [ ( 0, dummyWeightingsDict ), ( 1, dummyWeightingsDict ), ( 2, dummyWeightingsDict ) ]
-
-
-dummyServiceData : ServiceData
-dummyServiceData =
-    ServiceData "" "" "" ""
-
-
-servicesDict : Services
-servicesDict =
-    Dict.fromList [ ( 0, dummyServiceData ), ( 1, dummyServiceData ), ( 2, dummyServiceData ) ]
-
-
-zerosWeightingDict : WeightingsDict
-zerosWeightingDict =
-    Dict.fromList [ ( 0, 0 ), ( 1, 0 ), ( 2, 0 ) ]
-
-
-dummyWeightingsDict : WeightingsDict
-dummyWeightingsDict =
-    Dict.fromList [ ( 0, 0.8 ), ( 1, 0.9 ), ( 2, 0.3 ) ]
