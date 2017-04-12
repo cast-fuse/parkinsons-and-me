980ac5e06dbfcdc5fbd38c359d8743138e0d0c66
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-04-12 16:13:34 +0100

    Get rid of name on services page and add unique constraint for quote-service pairs in DB. #52 #51

--- web/elm/Views/Services.elm
+++ web/elm/Views/Services.elm
@@ -12,7 +12,7 @@ services : Model -> Html Msg
 services model =
     div []
         [ logo
-        , h2 [ class "blue mb4" ] [ text <| renderName model ]
+        , h2 [ class "blue mb4" ] [ text "Here you are, your tailored list of Parkinson's services" ]
         , div [ class "bg-blue pb6" ] (List.map renderService model.top3things)
         , div [ class "mw7 center mv4" ]
             [ h3 [ class "blue" ] [ text "If you'd like a copy of your results for futute reference, please enter your email" ]
@@ -20,20 +20,6 @@ services model =
             , button [ onClick SubmitEmail, class (Styles.buttonBlue ++ " mt3") ] [ text "submit" ]
             ]
         ]
-
-
-renderName : Model -> String
-renderName model =
-    let
-        title name =
-            "Here you are" ++ name ++ ", your tailored list of Parkinson's servivces"
-    in
-        case model.name of
-            Just name ->
-                title <| " " ++ name
-
-            Nothing ->
-                title ""
 
 
 renderService : ServiceData -> Html Msg
--- web/models/weight.ex
+++ web/models/weight.ex
@@ -8,9 +8,12 @@ defmodule What3things.Weight do
     belongs_to :service, What3things.Service
   end
 
+  @service_quote_error_message "Must be a unique service-quote pair"
+
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:weight, :quote_id, :service_id])
+    |> unique_constraint(:unique_service_quote_pair, name: :weights_quote_id_service_id_index, message: @service_quote_error_message)
     |> validate_required([:quote_id, :service_id, :weight])
   end
 end
