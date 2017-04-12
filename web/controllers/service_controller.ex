95c08ed34114347b3404d2e16c34826961fe0da2
Andrew MacMurray <a.macmurray@icloud.com>
2017-04-12 12:36:43 +0100

    can edit service and toggle if service is relevant for early onset information #50

--- web/controllers/service_controller.ex
+++ web/controllers/service_controller.ex
@@ -13,4 +13,18 @@ defmodule What3things.ServiceController do
 
     render conn, "edit.html", changeset: changeset, service: service
   end
+
+  def update(conn, %{"id" => service_id, "service" => service}) do
+    old_service = Repo.get(Service, service_id)
+    changeset = Service.changeset(old_service, service)
+
+    case Repo.update(changeset) do
+      {:ok, _service} ->
+        conn
+        |> put_flash(:info, "service updated")
+        |> redirect(to: service_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, service: old_service
+    end
+  end
 end
