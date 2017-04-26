08a7009ff3c8578e8f194a9cd715a1a279f21c38
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-04-26 16:24:02 +0100

    Refactor user_controller tests.

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,9 +2,12 @@ defmodule What3things.UserControllerTest do
   use What3things.ConnCase, async: true
   alias What3things.{Repo, User}
 
+  @dummy_user %{name: "Ivan", age_range: "under_forty", postcode: "sw99ng", email: "ivan@email.com"}
+  @dummy_user_stringified %{"name" => "Ivan", "age_range" => "under_forty", "postcode" => "sw99ng", "email" => "ivan@email.com"}
+
   describe "create/2" do
     test "Create and responds with a newly created user if attributes are valid" do
-      user = %{user: %{name: "Ivan", age_range: "under_forty", postcode: "sw99ng", email: "ivan@email.com"}}
+      user = %{user: @dummy_user}
 
       response = build_conn()
       |> post(user_path(build_conn(), :create), user)
@@ -13,22 +16,23 @@ defmodule What3things.UserControllerTest do
       created_user = Repo.get_by(User, name: "Ivan")
 
       expected = %{"data" =>
-        %{"name" => "Ivan", "age_range" => "under_forty", "postcode" => "sw99ng", "email" => "ivan@email.com", "id" => created_user.id}
+        Map.put(@dummy_user_stringified, "id", created_user.id)
       }
+
       assert response == expected
     end
 
     test "If user is already in database, does not create a new one" do
-      user = %{name: "Ivan", age_range: "under_forty", postcode: "sw99ng", email: "ivan@email.com"}
-      changeset = User.changeset(%User{}, user)
+      changeset = User.changeset(%User{}, @dummy_user)
       existing_user = Repo.insert!(changeset)
 
       response = build_conn()
-      |> post(user_path(build_conn(), :create), %{user: user})
+      |> post(user_path(build_conn(), :create), %{user: @dummy_user})
       |> json_response(200)
 
       expected = %{"data" =>
-      %{"age_range" => "under_forty", "email" => "ivan@email.com", "id" => existing_user.id, "name" => "Ivan", "postcode" => "sw99ng"}}
+        Map.put(@dummy_user_stringified, "id", existing_use.id)
+      }
 
       assert response == expected
     end
