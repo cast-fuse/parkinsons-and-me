121c56275520cd697ad71d9903193f73c0bcd286
Andrew MacMurray <a.macmurray@icloud.com>
2017-05-04 08:58:38 +0100

    outboundLink as reusable component

--- web/elm/Components/EarlyOnset.elm
+++ web/elm/Components/EarlyOnset.elm
@@ -2,13 +2,6 @@ module Components.EarlyOnset exposing (..)
 
 import Model exposing (..)
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-
-
-url : String
-url =
-    "https://www.parkinsons.org.uk/"
 
 
 earlyOnset : Html Msg
@@ -16,5 +9,4 @@ earlyOnset =
     div []
         [ p [] [ text "your rights and legal options" ]
         , p [] [ text "money grants and benefits" ]
-        , a [ href url, onClick <| TrackOutboundLink url ] [ text "Find out more" ]
         ]
--- web/elm/Components/Utils.elm
+++ web/elm/Components/Utils.elm
@@ -1,8 +1,16 @@
 module Components.Utils exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Model exposing (..)
 
 
 emptyDiv : Html msg
 emptyDiv =
     div [] []
+
+
+outboundLink : String -> String -> Html Msg
+outboundLink body url =
+    a [ href url, onClick <| TrackOutboundLink url ] [ text body ]
