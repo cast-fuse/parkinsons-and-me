a82e55a6860fff8da7b36820d0398aa4c861c740
Andrew MacMurray <a.macmurray@icloud.com>
2017-05-04 15:08:37 +0100

    aggregates view for user answers

--- web/controllers/analytics_controller.ex
+++ web/controllers/analytics_controller.ex
@@ -1,10 +1,18 @@
 defmodule What3things.AnalyticsController do
   use What3things.Web, :controller
-  alias What3things.{User}
+  alias What3things.{User, Answer}
+
+  def index(conn, %{"users" => "true"}) do
+    users = Repo.all(User)
+    render conn, "users.html", users: users
+  end
+
+  def index(conn, %{"answers" => "true"}) do
+    quote_answers = Repo.all(Answer.answer_aggregates)
+    render conn, "answers.html", quote_answers: quote_answers
+  end
 
   def index(conn, _params) do
-    users = Repo.all(User)
-
-    render conn, "index.html", users: users
+    render conn, "index.html"
   end
 end
