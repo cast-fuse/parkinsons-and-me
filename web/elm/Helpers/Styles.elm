c3dbce10fa79d8254e0b3c261d573e32109939e8
Andrew MacMurray <a.macmurray@icloud.com>
2017-05-09 13:11:41 +0100

    use classes helper in style helper file

--- web/elm/Helpers/Styles.elm
+++ web/elm/Helpers/Styles.elm
@@ -1,11 +1,6 @@
 module Helpers.Styles exposing (..)
 
 
-classes : List String -> String
-classes =
-    String.join " "
-
-
 button : String
 button =
     "ph3 pv2 f3 outline-0 pointer no-select sans-serif"
@@ -18,14 +13,19 @@ buttonClearHover =
 
 buttonClear : String
 buttonClear =
-    "bg-white b--green-blue ba bw1 " ++ button
+    classes [ "bg-white b--green-blue ba bw1", button ]
 
 
 buttonDisabled : String
 buttonDisabled =
-    "bg-light-gray gray bn " ++ button
+    classes [ "bg-light-gray gray bn", button ]
 
 
 inputField : String
 inputField =
     "w-100 f6 b--green-blue ba h2 outline-0 ph3 pv1"
+
+
+classes : List String -> String
+classes =
+    String.join " "
